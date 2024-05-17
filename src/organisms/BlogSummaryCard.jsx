55f5acd374cd7720adde295f8768957cb2f96597
mrFunkyWisdom <ensar.bavrk@gmail.com>
2024-05-18 01:49:47 +0200

    change index blog card

--- src/organisms/BlogSummaryCard.jsx
+++ src/organisms/BlogSummaryCard.jsx
@@ -14,32 +14,31 @@ export const BlogSummaryCard = ({ meta, slug }) => {
       <li
         key={meta.title}
         className={
-          'card card-side bg-base-100 mb-8 sm:flex-row flex-col my-12'
+          'card card-side bg-base-100 mb-8 sm:flex-row flex-col-reverse my-12'
         }>
-        <Image
-          style={{
-            objectFit: 'cover', // cover, contain, none
-            height: '240px',
-            minWidth: '240px',
-            borderRadius: '16px',
-          }}
-          className="sm:w-60 w-full"
-          src={meta.image}
-          alt={meta.alt || ''}
-          width={240}
-          height={240}
-        />
-        <div className="card-body py-0 px-0 sm:px-8 sm:mt-0 mt-6">
-          <H2 className="card-title">{meta.title}</H2>
+        <div className="card-body py-0 px-0 sm:mt-0 mt-6">
+          <Small className="text-textSecondary">Jan 31, 2022</Small>
+          <H2 className="card-title font-bold">{meta.title}</H2>
           <P className="my-4 text-secondary text-textSecondary">
             {meta.summary}
           </P>
-          <div className="flex mb-1">
-            <Small>12 views</Small>
-            <Small>6 min read</Small>
-            <Small>Jan 31, 2022</Small>
+          <div className="mb-1 flex">
+            <Small className="flex-grow-0 mr-4">6 min read</Small>
+            <Small className="flex-grow-0 ">12123123 views</Small>
           </div>
         </div>
+        <Image
+          style={{
+            objectFit: 'cover', // cover, contain, none
+            height: '220px',
+            minWidth: '220px',
+          }}
+          className="sm:w-60 w-full sm:rounded-r-xl rounded-xl sm:rounded-l-none"
+          src={meta.image}
+          alt={meta.alt || ''}
+          width={120}
+          height={120}
+        />
       </li>
     </Link>
   );
