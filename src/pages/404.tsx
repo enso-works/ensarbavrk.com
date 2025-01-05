eaddcd1c00543357d6a870816788674d3a1278e8
mrFunkyWisdom <ensar.bavrk@gmail.com>
2025-01-05 02:06:39 +0100

    improved 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,36 +1,46 @@
-import Error from 'next/error';
-import { useState, useEffect } from 'react';
 import Link from 'next/link';
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import { ArrowLeft } from 'lucide-react';
 
-export default function Page() {
-  const [gifUrl, setGifUrl] = useState('');
+interface NotFoundProps {
+  gifUrl: string;
+}
 
-  useEffect(() => {
-    const fetchRandomGif = async () => {
-      try {
-        const response = await fetch(
-          `https://api.giphy.com/v1/gifs/random?api_key=${process.env.NEXT_PUBLIC_GIPHY_API_KEY}&tag=confused&rating=g`
-        );
-        const data = await response.json();
-        setGifUrl(data.data.images.original.url);
-      } catch (error) {
-        console.error('Error fetching gif:', error);
-      }
+export const getStaticProps: GetStaticProps<NotFoundProps> = async () => {
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API_KEY}&tag=confused&rating=g`
+    );
+    const data = await response.json();
+    
+    return {
+      props: {
+        gifUrl: data.data.images.original.url,
+      },
+      // Revalidate every hour
+      revalidate: 3600,
     };
+  } catch (error) {
+    console.error('Error fetching gif:', error);
+    return {
+      props: {
+        gifUrl: '', // or a default fallback GIF URL
+      },
+    };
+  }
+};
 
-    fetchRandomGif();
-  }, []);
-
+export default function NotFound({ gifUrl }: NotFoundProps) {
   return (
     <div className="absolute top-0 left-0 right-0 bottom-0 flex flex-col items-center justify-center bg-[#313131] text-[#ffffff] overflow-hidden">
       {gifUrl && (
         <Image
           src={gifUrl}
           alt="Random confused gif"
-          layout="fill"
           objectFit="cover"
-          className="opacity-30"
+          className="opacity-50"
+          fill
           priority
         />
       )}
@@ -43,18 +53,8 @@ export default function Page() {
         </p>
         <Link
           href="/"
-          className="bg-[#00A7DC] text-white font-bold py-3 px-6 rounded-full text-lg hover:bg-[#0090C0] transition duration-300 ease-in-out inline-flex items-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5 mr-2"
-            viewBox="0 0 20 20"
-            fill="currentColor">
-            <path
-              fillRule="evenodd"
-              d="M9.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L7.414 9H15a1 1 0 110 2H7.414l2.293 2.293a1 1 0 010 1.414z"
-              clipRule="evenodd"
-            />
-          </svg>
+          className=" text-white font-bold py-3 px-6 rounded-full text-lg hover:bg-[#0090C0] transition duration-300 ease-in-out inline-flex items-center">
+          <ArrowLeft className="h-5 w-5 mr-2 animate-[pointing_1s_ease-in-out_infinite]" />
           Return to Safety
         </Link>
       </div>
